d5591ea37410575f7d3463949cd2699f371fec7f
Dr Nic Williams <drnicwilliams@gmail.com>
2015-04-19 21:33:36 -0700

    default to manifest.yml

--- dev1/make_manifest_and_save.sh
+++ dev1/make_manifest_and_save.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
-output_manifest=$1; shift
-if [[ "${output_manifest}X" == "X" ]]; then
-  echo "USAGE: make_manifest_and_save.sh manifest.yml [args.yml to.yml spiff.yml merge.yml]"
-  exit 1
-fi
+manifest=${manifest:-"manifest.yml"}
 
 set -e
 
 cd $DIR
-./make_manifest.sh $@ > $output_manifest
+./make_manifest.sh $@ > $manifest
