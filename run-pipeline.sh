2c9cc14de595bc27371b0a33418c986eaa7652dc
Dr Nic Williams <drnicwilliams@gmail.com>
2015-04-19 19:43:15 -0700

    script to load/run pipeline job

---
+++ run-pipeline.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+stub=$1; shift
+set -e
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+export ATC_URL=${ATC_URL:-"http://192.168.100.4:8080"}
+echo "Tutorial $(basename $DIR)"
+echo "Concourse API $ATC_URL"
+
+realpath() {
+    [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+}
+
+usage() {
+  echo "USAGE: run-pipeline.sh credentials.yml"
+  exit 1
+}
+
+if [[ "${stub}X" == "X" ]]; then
+  usage
+fi
+stub=$(realpath $stub)
+if [[ ! -f ${stub} ]]; then
+  usage
+fi
+
+pushd $DIR
+  yes y | fly configure -c pipeline.yml --vars-from ${stub}
+  curl $ATC_URL/jobs/job-deploy/builds -X POST
+  fly watch -j job-deploy
+popd
