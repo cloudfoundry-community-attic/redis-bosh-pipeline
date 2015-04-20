8b6d0b70cd391cc428d328fddf592ee98f8535ab
Dr Nic Williams <drnicwilliams@gmail.com>
2015-04-20 16:59:13 -0700

    staging starts as just pipeline specific spiff templates

---
+++ staging/bin/unpack_assets.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+assets_tgz=$1; shift
+
+set -e
+
+# if [[ ! -f ${assets_tgz} ]]; then
+#   echo "USAGE: ./bin/unpack_assets.sh path/to/candidate-assets-x.y.z.tgz"
+#   exit 1
+# fi
+
+ls -opR ${assets_tgz}
+
+# tar xfz ${assets_tgz}
+#
+# ls -opR .
