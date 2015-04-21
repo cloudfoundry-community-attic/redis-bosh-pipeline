487e5aec072f03bb465bd273335af1600a47a8ec
Dr Nic Williams <drnicwilliams@gmail.com>
2015-04-20 17:06:47 -0700

    unpack candidate tgz

--- staging/bin/unpack_assets.sh
+++ staging/bin/unpack_assets.sh
@@ -4,13 +4,13 @@ assets_tgz=$1; shift
 
 set -e
 
-# if [[ ! -f ${assets_tgz} ]]; then
-#   echo "USAGE: ./bin/unpack_assets.sh path/to/candidate-assets-x.y.z.tgz"
-#   exit 1
-# fi
+if [[ ! -f ${assets_tgz} ]]; then
+  echo "USAGE: ./bin/unpack_assets.sh path/to/candidate-assets-x.y.z.tgz"
+  exit 1
+fi
 
 ls -opR ${assets_tgz}
 
-# tar xfz ${assets_tgz}
-#
-# ls -opR .
+tar xfz ${assets_tgz}
+
+ls -opR pipeline-assets/
