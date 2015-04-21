b389eb90d220fd2b51e42979581e4e4bc4d2dee3
Dr Nic Williams <drnicwilliams@gmail.com>
2015-04-20 17:16:10 -0700

    unpack integration templates & bin into staging folder

--- staging/bin/unpack_assets.sh
+++ staging/bin/unpack_assets.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 assets_tgz=$1; shift
+pipeline_stage_dir=$1; shift
 
 set -e
 
 tar xfz ${assets_tgz}
 
 ls -opR pipeline-assets/
+
+cp -r pipeline-assets/bin $pipeline_stage_dir/
+cp -r pipeline-assets/templates $pipeline_stage_dir/
