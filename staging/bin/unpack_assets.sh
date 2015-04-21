46d9c45adb196732d1cf32a4f263ec2da58334c3
Dr Nic Williams <drnicwilliams@gmail.com>
2015-04-20 17:09:42 -0700

    unpack.sh try unpacking

--- staging/bin/unpack_assets.sh
+++ staging/bin/unpack_assets.sh
@@ -4,15 +4,6 @@ assets_tgz=$1; shift
 
 set -e
 
-echo assets tgz: ${assets_tgz}
-
-if [[ ! -f ${assets_tgz} ]]; then
-  echo "USAGE: ./bin/unpack_assets.sh path/to/candidate-assets-x.y.z.tgz"
-  exit 1
-fi
-
-ls -opR ${assets_tgz}
-
 tar xfz ${assets_tgz}
 
 ls -opR pipeline-assets/
