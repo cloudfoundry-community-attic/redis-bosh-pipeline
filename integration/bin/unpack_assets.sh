379d351edc400def99fd64863597f44b0d8d0927
Dr Nic Williams <drnicwilliams@gmail.com>
2015-04-23 17:52:48 -0700

    optimize unpack code

--- integration/bin/unpack_assets.sh
+++ integration/bin/unpack_assets.sh
@@ -10,13 +10,8 @@ tar xfz ${assets_tgz}
 ls -opR pipeline-assets/
 
 rm -rf $pipeline_stage_dir/bin
-cp -r pipeline-assets/bin $pipeline_stage_dir/
-
 rm -rf $pipeline_stage_dir/templates
-cp -r pipeline-assets/templates $pipeline_stage_dir/
-
 rm -rf $pipeline_stage_dir/releases
-cp -r pipeline-assets/releases $pipeline_stage_dir/
-
 rm -rf $pipeline_stage_dir/stemcell
-cp -r pipeline-assets/stemcell $pipeline_stage_dir/
+
+cp -r pipeline-assets/* $pipeline_stage_dir/
