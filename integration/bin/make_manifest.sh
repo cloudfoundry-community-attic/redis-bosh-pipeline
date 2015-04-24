0114a4c6cfa5fc4690549f0b57441807ac5550b9
Dr Nic Williams <drnicwilliams@gmail.com>
2015-04-23 17:05:42 -0700

    pass release/stemcell metadata into make_manifest and store in git repo

--- integration/bin/make_manifest.sh
+++ integration/bin/make_manifest.sh
@@ -3,6 +3,19 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 cd $DIR/..
+
+REDIS_VERSION=$(cat releases/redis/version)
+STEMCELL_VERSION=$(cat stemcell/version)
+
+cat >manifests/stub.yml <<EOF
+---
+meta:
+  release_versions:
+    redis: $REDIS_VERSION
+  stemcell:
+    version: $STEMCELL_VERSION
+EOF
+
 spiff merge \
   templates/redis/deployment.yml \
   templates/redis/jobs.yml \
@@ -13,4 +26,5 @@ spiff merge \
   environment/properties.yml \
   environment/director.yml \
   environment/name.yml \
-  environment/scaling.yml
+  environment/scaling.yml \
+  manifests/stub.yml
