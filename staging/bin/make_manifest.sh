12b7d20bb360eb5da67fe8e089ec4af7e4faacec
Concourse <nobody@concourse.ci>
2015-04-24 00:29:07 +0000

    [staging] Update manifest

--- staging/bin/make_manifest.sh
+++ staging/bin/make_manifest.sh
@@ -3,6 +3,20 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 cd $DIR/..
+
+REDIS_VERSION=$(cat releases/redis/version)
+STEMCELL_VERSION=$(cat stemcell/version)
+
+mkdir -p manifests
+cat >manifests/stub.yml <<EOF
+---
+meta:
+  release_versions:
+    redis: $REDIS_VERSION
+  stemcell:
+    version: $STEMCELL_VERSION
+EOF
+
 spiff merge \
   templates/redis/deployment.yml \
   templates/redis/jobs.yml \
@@ -13,4 +27,5 @@ spiff merge \
   environment/properties.yml \
   environment/director.yml \
   environment/name.yml \
-  environment/scaling.yml
+  environment/scaling.yml \
+  manifests/stub.yml
