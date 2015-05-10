d39f62b39d8e8891956f84c55fbdecd8fc7bb9ac
Dr Nic Williams <drnicwilliams@gmail.com>
2015-05-09 19:24:09 -0700

    no manifest required for import packages

--- try-anything/bin/import_compiled_packages.sh
+++ try-anything/bin/import_compiled_packages.sh
@@ -8,5 +8,5 @@ set -e
 # bosh-warden-boshlite-ubuntu-trusty-go_agent/redis/redis-9.1-bosh-warden-boshlite-ubuntu-trusty-go_agent-2776.tgz
 compiled_package=$(ls compiled_packages/*.tgz)
 
-bosh -t ${bosh_target} -u ${bosh_username} -p ${bosh_password} -d ${manifest} \
+bosh -t ${bosh_target} -u ${bosh_username} -p ${bosh_password} \
   import compiled_packages ${compiled_package}
