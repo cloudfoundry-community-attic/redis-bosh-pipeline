af5deec58f192dfcd66db89550d326879a328376
Dr Nic Williams <drnicwilliams@gmail.com>
2015-05-09 19:20:24 -0700

    import compiled packages from same folder structure as exported

--- try-anything/bin/import_compiled_packages.sh
+++ try-anything/bin/import_compiled_packages.sh
@@ -6,7 +6,7 @@ set -e
 
 # compiled packages are in subfolder:
 # bosh-warden-boshlite-ubuntu-trusty-go_agent/redis/redis-9.1-bosh-warden-boshlite-ubuntu-trusty-go_agent-2776.tgz
-compiled_package=$(ls compiled-package-redis/*/*/*.tgz)
+compiled_package=$(ls compiled_packages/*/*/*.tgz)
 
 bosh -t ${bosh_target} -u ${bosh_username} -p ${bosh_password} -d ${manifest} \
   import compiled_packages ${compiled_package}
