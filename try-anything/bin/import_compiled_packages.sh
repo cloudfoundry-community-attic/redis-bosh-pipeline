165067b97260593af668d8d1b036c97713a07111
Dr Nic Williams <drnicwilliams@gmail.com>
2015-05-09 19:31:55 -0700

    upload stemcell & releases before compiled packages

--- try-anything/bin/import_compiled_packages.sh
+++ try-anything/bin/import_compiled_packages.sh
@@ -4,9 +4,16 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $DIR/..
 set -e
 
-# compiled packages are in subfolder:
-# bosh-warden-boshlite-ubuntu-trusty-go_agent/redis/redis-9.1-bosh-warden-boshlite-ubuntu-trusty-go_agent-2776.tgz
-compiled_package=$(ls compiled_packages/*.tgz)
+bosh_() {
+  bosh -t ${bosh_target} -u ${bosh_username} -p ${bosh_password} $@
+}
 
-bosh -t ${bosh_target} -u ${bosh_username} -p ${bosh_password} \
-  import compiled_packages ${compiled_package}
+stemcell=$(ls stemcell/*.tgz)
+bosh_ upload stemcell ${stemcell}
+
+release=$(ls releases/redis/*.tgz)
+bosh_ upload release ${release}
+
+
+compiled_package=$(ls compiled_packages/*.tgz)
+bosh_ import compiled_packages ${compiled_package}
